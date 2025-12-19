f0d7a770fc023e769448e4d4188c0d19904841a9
Aaron Tanguma <the_guma@yahoo.com>
2025-12-18 18:48:58 -0700

    created Student class and created properties

---
+++ TeacherGradeBook/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace TeacherGradeBook;
+
+public class Student
+{
+    // create properties that will give the Student blueprint their characteristics
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public char Grades { get; set; }
+}
+
